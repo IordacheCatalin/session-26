65cc863bc6aa6b78f1a072f246c6e0afb05b0efa
Iordache Catalin <iordache.catalin1986@gmail.com>
2022-07-07 18:08:17 +0300

    Sesion 27

--- js/register.js
+++ js/register.js
@@ -1,52 +1,45 @@
-const registerForm= document.querySelector("form");
+const registerForm = document.querySelector("form");
 
+const registerUser = (event) => {
+  event.preventDefault();
 
-const registerUser = event => {
-    event.preventDefault();
+  const firstName = document.querySelector(`input[name="firstName"]`).value;
+  const lastName = document.querySelector(`input[name="lastName"]`).value;
+  const email = document.querySelector(`input[name="email"]`).value;
+  const password = document.querySelector(`input[name="password"]`).value;
 
-    const firstName = document.querySelector(`input[name="firstName"]`).value;
-    const lastName = document.querySelector(`input[name="lastName"]`).value;
-    const password = document.querySelector(`input[name="password"]`).value;
-    const email = document.querySelector(`input[name="email"]`).value;
-    
-    const payload = JSON.stringify({
-        //firstName: firstName;
-        firstName,
-        lastName,
-        email,
-        password,
+  const payload = JSON.stringify({
+    firstName,
+    lastName,
+    email,
+    password,
+  });
 
-    });
-
-    fetch("https://backend-curs.herokuapp.com/api/users/register", {
-        method: "POST",
-        headers: {
-            "Content-Type" : "application/json"
-        },
-        body: payload,
-    })
+  fetch("https://backend-curs.herokuapp.com/api/users/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: payload,
+  })
     .then((res) => res.json())
-    .then((res) => console.info(res))
-    .then((err) => console(err));
+    .then((res) => {
+      // another fetch for login!
+    })
+    .catch((err) => console.error(err));
 
-
- // const formInputs = document.querySelectorAll("input");
-// console.log(formInputs);
- 
+  //   const formInputs = document.querySelectorAll("input");
+  //   console.log(formInputs);
 };
 
-
-registerForm.addEventListener("submit",registerUser);
+registerForm.addEventListener("submit", registerUser);
 
 // const formInputs = document.querySelectorAll("input");
 // console.log(formInputs);
 
-//[{firstName: ""}, {lastName: ""}]
-
-//Bulk process inputs & transform to structure
+// Bulk process inputs & transform to structure
 // const processedInputs = [];
 // formInputs.forEach((el) => {
-//     processedInputs.push({ [el.name]: el.value });
+//   processedInputs.push({ [el.name]: el.value });
 // });
-
 // console.log(processedInputs);
